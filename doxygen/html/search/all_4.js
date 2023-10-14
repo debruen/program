6bd5a410838a756cb3bc0e28b61d8cb3a4629b65
Florian de Brün <44509713+debruen@users.noreply.github.com>
2023-10-14 23:03:47 +0200

    Delete doxygen directory

--- doxygen/html/search/all_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['program_5',['Program',['../class_program.html',1,'Program'],['../class_program.html#aaefaa0df08f3484476fc4d61e97acbdc',1,'Program::Program()']]]
-];
--- doxygen/html/search/functions_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['program_11',['Program',['../class_program.html#aaefaa0df08f3484476fc4d61e97acbdc',1,'Program']]]
-];
